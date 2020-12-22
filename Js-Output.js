a3304d95eb93c0ccdfe7f44bbf437d0fa8acce88
ayub6717 <ayub15-6717@diu.edu.bd>
2020-12-22 17:10:30 +0600

    First commit

---
+++ Js-Output.js
@@ -0,0 +1,14 @@
+// Using innerHTML
+document.getElementById("demo").innerHTML = 3 + 9;
+
+// Using document.write()
+document.write(9+11);
+
+// Using window.alert()
+window.alert(12+11);
+alert(19+3);
+
+// Using console.log()
+console.log(21+22);
+
+window.print();
